1b97e8f682891454e461d624cb318526ea079d31
Yash Patidar <yashpatidar964@gmail.com>
2023-04-04 16:04:47 +0530

    Add - Question 4 of Set 2

--- reactPracticeSet2/reactquestionset2/src/App.jsx
+++ reactPracticeSet2/reactquestionset2/src/App.jsx
@@ -2,19 +2,23 @@ import "./App.css";
 import { Question1 } from "./Question1";
 import { Question2 } from "./Question2";
 import { Userprofile } from "./Question3";
+import {products} from "./Products";
+import { Question4 } from "./Question4";
 
 const userData = {
   name: 'John',
   age: 25,
   email: 'john@example.com',
 }
-
 function App() {
   return (
+    
     <div className="App">
       <Question1 nameprop={"Yash"} />
       <Question2 productname={"Macbook"} productprice={99990}/>
       <Userprofile userdata={userData}/>
+      <Question4 productlist={products}/>
+
     </div>
   );
 }
