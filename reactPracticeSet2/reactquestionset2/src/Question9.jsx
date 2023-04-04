bc9e9ab97638d1b1ee9e76dedf817a688400dab9
Yash Patidar <yashpatidar964@gmail.com>
2023-04-05 00:40:35 +0530

    Add - Question 9 and 10 of set 2

--- reactPracticeSet2/reactquestionset2/src/Question9.jsx
+++ reactPracticeSet2/reactquestionset2/src/Question9.jsx
@@ -1,30 +1,79 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export const Question9 =()=>{
-    const red = '#EE4B2B'
-const blue = '#89CFF0'
-const green = '#7FFFD4'
-const [showcode,setshowcode] = useState(false)
+export const Question9 = () => {
+  const [code, setcode] = useState(null);
+  const colors = [
+    { name: "Red", value: "#EE4B2B" },
+    { name: "Blue", value: "#89CFF0" },
+    { name: "Green", value: "#7FFFD4" },
+  ];
+  const clickhandler = (code) => {
+    setcode(code);
+  };
 
-const showcodehandler1 = () =>{
-    setshowcode(!showcode)
-}
-const showcodehandler2 = () =>{
-    setshowcode(!showcode)
-}
-const showcodehandler3 = () =>{
-    setshowcode(!showcode)
-}
+  return (
+    <div>
+      <h2>Question 9</h2>
+      <div>
+        {colors.map((item) => (
+          <button  onClick={() => clickhandler(item)}>{item.name}</button>
+        ))}
 
-    return(
-        <div>
-            <h2>Question 9 </h2>
-            <button onClick={showcodehandler1}>Red</button>
-            <p style={{display : showcode ? "block":"none"}}>{red}</p>
-            <button onClick={showcodehandler2}>Blue</button>
-            <p style={{display : showcode ? "block":"none"}}>{blue}</p>
-            <button onClick={showcodehandler3}>Green</button>
-            <p style={{display : showcode ? "block":"none"}}>{green}</p>
-        </div>
-    )
-}
+        <p>HEX code: {code.value}</p>
+      </div>
+          </div>
+  );
+
+  //     const [selectedColor, setSelectedColor] = useState(null);
+  //   const colors = [
+  //     { name: 'Red', value: '#EE4B2B' },
+  //     { name: 'Blue', value: '#89CFF0' },
+  //     { name: 'Green', value: '#7FFFD4' },
+  //   ];
+
+  //   const handleColorClick = (color) => {
+  //     setSelectedColor(color);
+  //   };
+
+  //   return (
+  //     <div>
+  //       <h2>Select a color:</h2>
+  //       <div>
+  //         {colors.map((color) => (
+  //           <button
+  //             key={color.name}
+  //             style={{ backgroundColor: color.value }}
+  //             onClick={() => handleColorClick(color)}
+  //           >
+  //             {color.name}
+  //           </button>
+  //         ))}
+  //       </div>
+  //       {selectedColor && (
+  //         <div>
+  //           <p>HEX code: {selectedColor.value}</p>
+  //         </div>
+  //       )}
+  //     </div>
+  //   );
+//   const red = '#EE4B2B'
+//   const blue = '#89CFF0'
+//   const green = '#7FFFD4'
+
+//   const [showcode,setshowcode] = useState(null)
+  
+//   const showcodehandler = () =>{
+//       setshowcode((showcode) => showcode =red )
+//   }
+//       return(
+//           <div>
+//               <h2>Question 9 </h2>
+//               <button onClick={showcodehandler}>Red</button>
+//               <p >{showcode}</p>
+//               <button onClick={showcodehandler}>Blue</button>
+//               <p >{showcode}</p>
+//               <button onClick={showcodehandler}>Green</button>
+//               <p >{ showcode}</p>
+//           </div>
+//       )
+ };
