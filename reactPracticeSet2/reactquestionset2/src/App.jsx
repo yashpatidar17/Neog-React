796a88e8d37f2825ad43610d10bb6d2595b05b8a
Yash Patidar <yashpatidar964@gmail.com>
2023-04-04 15:23:16 +0530

    Add - question 2 of set 2

--- reactPracticeSet2/reactquestionset2/src/App.jsx
+++ reactPracticeSet2/reactquestionset2/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Question1 } from "./Question1";
+import { Question2 } from "./Question2";
 
 function App() {
   return (
     <div className="App">
       <Question1 nameprop={"Yash"} />
+      <Question2 productname={"Macbook"} productprice={99990}/>
     </div>
   );
 }
