dd1e7d4f3e5670807cc0787bf775557f471398ff
Yash Patidar <60218098+yashpatidar17@users.noreply.github.com>
2023-04-24 23:57:16 +0530

    deleted set 3

--- reactPracticeSet3/reactquestionset3/src/Question3.js
+++
@@ -1,9 +0,0 @@
-export const Question3 = ({itemlist})=>{
-    return(
-
-        <div>
-            <h2>Question 3</h2>
-            {itemlist.reduce((acc,curr)=>(curr.magnitude %2 ===0 ? [...acc,<h1>{curr.name}</h1>]:acc),[])}
-        </div>
-    )
-}
