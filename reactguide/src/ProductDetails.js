63ad6954200c23b70de0b31c320ca87644c466cd
Yash Patidar <60218098+yashpatidar17@users.noreply.github.com>
2023-04-24 23:52:58 +0530

    deleted react guide

--- reactguide/src/ProductDetails.js
+++
@@ -1,7 +0,0 @@
-export const productDetails = {
-    id: "AC1023",
-    name: "Air Conditioner",
-    price: 29600,
-    specification: "1 Ton, 4 Star Rating",
-    warranty: "5 Years Compressor Warranty"
-};
--- reactguide/src/fruits.js
+++
@@ -1,8 +0,0 @@
-export const fruits = [
-    { id: 1, name: "Apple", price: 20 },
-    { id: 2, name: "Orange", price: 54 },
-    { id: 3, name: "Grapes", price: 30 },
-    { id: 4, name: "Pineapple", price: 70 }
-  ];
-
-  
