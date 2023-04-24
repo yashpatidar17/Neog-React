56fab8fc069ae9978b5fbf6655c9d5a4f0a4688b
Yash Patidar <60218098+yashpatidar17@users.noreply.github.com>
2023-04-24 23:56:33 +0530

    Deleted set 3

--- reactPracticeSet2/reactquestionset2/src/Question2.jsx
+++
@@ -1,9 +0,0 @@
-export const Question2 = ({productname,productprice})=>{
-    return(
-        <div>
-            <h2>Question 2</h2>
-            <p >Product Name : <span style ={{color: 'blue',fontWeight: 'bold'}}>{productname}</span></p>
-            <p>Product Price : <span style={{color: 'green',fontStyle: 'italic'}}>{productprice}</span></p>
-        </div>
-    )
-}
