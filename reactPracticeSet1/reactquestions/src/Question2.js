260b8f879123da937eed27745db0e34304d8880f
Yash Patidar <60218098+yashpatidar17@users.noreply.github.com>
2023-04-24 23:54:09 +0530

    deleted set 1

--- reactPracticeSet1/reactquestions/src/Question2.js
+++
@@ -1,8 +0,0 @@
-export const Question2 = ({backColor,color,brdradius,padd}) =>{
-    return(
-        <div>
-            <h2>Question 2</h2>
-            <button style={{padding:padd,backgroundColor:backColor,color:color,borderRadius:brdradius}}>Start</button>
-        </div>
-    )
-}
